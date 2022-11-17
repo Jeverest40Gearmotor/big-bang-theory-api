4bbb9132229f411e9d782e8282e657ef65dd18a2
Jeremiah Short <jeremiah.short@mrjaffesclass.com>
2022-11-17 12:51:07 -0800

    Added grabbing specific episodes from episode list

--- functions/router.js
+++ functions/router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const episodes = require('./routes/episodes');
+const episodeIndex = require('./routes/episodeIndex');
 
 router.get("/episodes", episodes);
+router.get("/episode-index/:index", episodeIndex);
 
 module.exports = router;
 
