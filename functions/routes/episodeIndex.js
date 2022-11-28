505fd39e411433c20fd115c8c6f2cc4be25a4d2a
Jeremiah Short <jeremiah.short@mrjaffesclass.com>
2022-11-28 12:59:18 -0800

    Added index validation to index route

--- functions/routes/episodeIndex.js
+++ functions/routes/episodeIndex.js
@@ -1,11 +1,19 @@
 const bbt = require('big-bang-theory');
 
 const episodeIndex = (req, res) => {
-  res.status(200).json({
+  if((req.params.index < bbt._embedded.episodes.length)&&(req.params.index >= 0)){
+    res.status(200).json({
     success: true,
     index: req.params.index,
     data: bbt._embedded.episodes[req.params.index]
-  })
+    })
+  } else {
+    res.status(400).json({
+    success: false,
+    index: req.params.index,
+    message: "Index value is out of range. There are only "+bbt._embedded.episodes.length+" episodes of this show."
+    })
+  }
 }
 
 module.exports = episodeIndex;
